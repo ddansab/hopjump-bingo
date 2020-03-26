d9002fd871384d0347bd18e02a3d054781ec0ec1
Dave <ddansab@gmail.com>
2020-03-26 19:35:09 -0400

    fix deploy??

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,10 @@ module.exports = {
   siteMetadata: {
     title: "Hopjump Bingo!",
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+    },
+  ],
 }
